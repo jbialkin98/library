f772b85e45f75e73812920b22fafd3ac654585cb
Josh Bialkin <jbialkin98@gmail.com>
2022-11-01 12:58:43 -0400

    Dynamically create divs to show books

--- code.js
+++ code.js
@@ -12,13 +12,48 @@ function addBookToLibrary(entry) {
     myLibrary.push(entry);
 }
 
+const bookOne = document.querySelector(".book-one");
+
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+
 console.log(myLibrary[0]);
 myLibrary.forEach((book) => {
+    bookTitle(book);
+    bookAuthor(book);
+    bookPages(book);
+    bookRead(book);
     console.log(book.title);
     console.log(book.author);
     console.log(book.pages);
     console.log(book.read);
 });
-const bookOne = document.querySelector(".book-one");
-bookOne.textContent = theHobbit.title;
+
+// bookOne.textContent = theHobbit.title;
+
+function bookTitle(book) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('newDiv');
+    bookOne.appendChild(newDiv);
+    newDiv.textContent = book.title;
+}
+
+function bookAuthor(book) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('newDiv');
+    bookOne.appendChild(newDiv);
+    newDiv.textContent = book.author;
+}
+
+function bookPages(book) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('newDiv');
+    bookOne.appendChild(newDiv);
+    newDiv.textContent = book.pages;
+}
+
+function bookRead(book) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('newDiv');
+    bookOne.appendChild(newDiv);
+    newDiv.textContent = book.read;
+}
